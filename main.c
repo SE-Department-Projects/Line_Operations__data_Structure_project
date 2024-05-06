4a47314e2d092c419b87d53b65fdafd22202375e
sshahdashraff <shashraf214@gmail.com>
2024-05-06 23:34:05 +0300

    .

--- main.c
+++ main.c
@@ -2,75 +2,67 @@
 #include <stdlib.h>
 #include "DoubleLinkedList.h"
 
-
-void main(){
-
-
+int main() {
     DubList l;
-
     ListEntry arr[500];
     int option = 0;
     int diff;
 
-    while(option != 2){
-
+    while (option != 2) {
         printf("\n1=> Enter text\n");
         printf("2=> Exit\n");
         printf("answer: ");
 
-        scanf("%d",&option);
+        char input;
+        scanf(" %c", &input);
+        option = input - '0';
 
-        if(option == 2)
-            break;
-        
-        if(option != 1){
-            printf("Enter a valid answer\n");
+        if (option != 1 && option != 2) {
+            printf("Enter a valid option (1 or 2)\n");
+            while (getchar() != '\n');
             continue;
         }
 
-        printf("Enter text: ");
-        scanf("%s",&arr);
+        if (option == 1) {
+            printf("Enter text: ");
+            scanf("%s", arr);
 
-        CreateList(&l);
+            CreateList(&l);
 
-        for(int i = 0; arr[i] != '\0' ; i++){
+            for(int i = 0; arr[i] != '\0' ; i++){
             InsertList(&l,arr[i],i);
         }
 
-        if(!FindColon(&l))
-            printf("N");
-        else{
-            diff = Difference(&l);
+            if (!FindColon(&l))
+                printf("N");
+            else {
+                diff = Difference(&l);
 
-            if(diff > 0) //left > right
-            {
-                printf("L");
-            }
-
-            else if (diff < 0) // right > left
-            {
-                if(IsPartOfRight(&l))
-                    printf("P");
-                else if(IsFoundInRight(&l))
-                    printf("Q");
-                else
-                    printf("R");
-            }
-
-            else // right == left
-            {
-                if(IsIdentical(&l) || IsMirror(&l)){
-                    if(IsIdentical(&l))
-                        printf("S\n");
-                    if(IsMirror(&l))
-                        printf("M");
+                if (diff > 0) //left > right
+                {
+                    printf("L");
+                } else if (diff < 0) // right > left
+                {
+                    if (IsPartOfRight(&l))
+                        printf("P");
+                    else if (IsFoundInRight(&l))
+                        printf("Q");
+                    else
+                        printf("R");
+                } else // right == left
+                {
+                    if (IsIdentical(&l) || IsMirror(&l)) {
+                        if (IsIdentical(&l))
+                            printf("S\n");
+                        if (IsMirror(&l))
+                            printf("M");
+                    } else
+                        printf("D");
                 }
-                else
-                    printf("D");
             }
+            DestroyList(&l);
         }
-        DestroyList(&l);
-
     }
 
+    return 0; // Exit the program
 }
